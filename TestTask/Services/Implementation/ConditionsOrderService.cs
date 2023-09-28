da66b0292c9d73b7390f90adef5592290b743ac4
nikita venediktov <nikben2105@gmail.com>
2023-09-28 14:39:22 +0300

    task's done

---
+++ TestTask/Services/Implementation/ConditionsOrderService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementation
+{
+    public class ConditionsOrderService : IOrderService
+    {
+        private ApplicationDbContext dbContext;
+
+        public ConditionsOrderService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return dbContext.Orders.OrderByDescending(o => o.Price).FirstOrDefaultAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return dbContext.Orders.Where(o => o.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementation/ConditionsUserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementation
+{
+    public class ConditionsUserService : IUserService
+    {
+        private ApplicationDbContext dbContext;
+
+        public ConditionsUserService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<User> GetUser()
+        {
+            return dbContext.Users.Include(u => u.Orders).OrderByDescending(u => u.Orders.Count).FirstOrDefaultAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return dbContext.Users.Where(u => u.Status == Enums.UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
